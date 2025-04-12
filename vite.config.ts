5a34b9454a9db8ee9e5adc8f6870dd85d72c4e51
Dillion Megida <dillionmegida@gmail.com>
2025-04-12 12:28:28 +0200

    improve build step for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ include: ['src'] })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`
+    },
+    minify: 'terser',
+    terserOptions: {
+      format: {
+        comments: false,
+        ecma: 2020,
+      },
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+        compact: true
+      }
+    }
+  }
 });
