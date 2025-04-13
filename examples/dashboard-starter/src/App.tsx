d1a2cd0bf143091ec2b705b2ab6d09dd4fc43f77
Dillion Megida <dillionmegida@gmail.com>
2025-04-14 00:47:19 +0200

    move tour component to root in example

--- examples/dashboard-starter/src/App.tsx
+++ examples/dashboard-starter/src/App.tsx
@@ -2,12 +2,43 @@ import { Toaster } from 'sonner';
 import { RouterProvider } from 'react-router-dom';
 import { browserRouter } from './routes/browserRouter';
 import '@dillionmegida/react-tour/dist/style.css';
+import { Tour } from '@dillionmegida/react-tour';
 
 function App() {
   return (
     <div className="fade-in">
       <RouterProvider router={browserRouter} />
       <Toaster />
+      <Tour
+        stepObj={{
+          onboardings: [
+            {
+              target: '.users-stat-card',
+              content: 'Hello',
+            },
+            {
+              target: '.ant-menu-item a[href="/users"]',
+              content: 'You can find all users here.',
+            },
+            {
+              target: '.posts-stat-card',
+              content: 'Hello',
+            },
+            {
+              target: '.users-list',
+              content: 'Here you can find the users.',
+            },
+            {
+              target: '.users-list',
+              content: 'Also a few extra things.',
+            },
+            {
+              target: '.reviews-table',
+              content: 'Here you can find the reviews from everyone.',
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
--- examples/dashboard-starter/src/components/dashboard/index.tsx
+++ examples/dashboard-starter/src/components/dashboard/index.tsx
@@ -26,7 +26,6 @@ import http from '../../utils/http';
 import { apiRoutes } from '../../routes/api';
 import { handleErrorResponse } from '../../utils';
 import { Review } from '../../interfaces/models/review';
-import { Tour } from '@dillionmegida/react-tour';
 
 const breadcrumb: BreadcrumbProps = {
   items: [
@@ -96,234 +95,196 @@ const Dashboard = () => {
   };
 
   return (
-    <>
-      <Tour
-        stepObj={{
-          onboardings: [
-            {
-              target: '.users-stat-card',
-              content: 'Hello',
-            },
-            {
-              target: '.posts-stat-card',
-              content: 'Hello',
-            },
-          ],
-        }}
-      />
-      <BasePageContainer breadcrumb={breadcrumb} transparent={true}>
-        <Row gutter={24}>
-          <Col
-            className="users-stat-card"
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={AiOutlineTeam} />}
-              title="Users"
-              number={12}
-            />
-          </Col>
-          <Col
-            className="posts-stat-card"
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={MdOutlineArticle} />}
-              title="Posts"
-              number={100}
-            />
-          </Col>
-          <Col
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={BiPhotoAlbum} />}
-              title="Albums"
-              number={100}
-            />
-          </Col>
-          <Col
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={MdOutlinePhoto} />}
-              title="Photos"
-              number={500}
-            />
-          </Col>
-          <Col
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={BiCommentDetail} />}
-              title="Comments"
-              number={500}
-            />
-          </Col>
-          <Col
-            xl={6}
-            lg={6}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <StatCard
-              loading={loading}
-              icon={<Icon component={AiOutlineStar} />}
-              title="Reviews"
-              number={100}
-            />
-          </Col>
-          <Col
-            xl={12}
-            lg={12}
-            md={24}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <Card bordered={false} className="w-full h-full cursor-default">
-              <StatisticCard.Group direction="row">
-                <StatisticCard
-                  statistic={{
-                    title: 'XYZ',
-                    value: loading ? 0 : 123,
-                  }}
-                />
-                <StatisticCard
-                  statistic={{
-                    title: 'Progress',
-                    value: 'ABC',
-                  }}
-                  chart={
-                    <Progress
-                      className="text-primary"
-                      percent={loading ? 0 : 75}
-                      type="circle"
-                      size={'small'}
-                      strokeColor={CONFIG.theme.accentColor}
-                    />
-                  }
-                  chartPlacement="left"
-                />
-              </StatisticCard.Group>
-            </Card>
-          </Col>
-          <Col
-            xl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <Card bordered={false} className="w-full h-full cursor-default">
-              <List
-                loading={loading}
-                itemLayout="horizontal"
-                dataSource={users}
-                renderItem={(user) => (
-                  <List.Item>
-                    <List.Item.Meta
-                      avatar={
-                        <Avatar
-                          shape="circle"
-                          size="small"
-                          src={
-                            <LazyImage
-                              src={user.avatar}
-                              placeholder={
-                                <div className="bg-gray-100 h-full w-full" />
-                              }
-                            />
-                          }
-                        />
-                      }
-                      title={`${user.first_name} ${user.last_name}`}
-                      description={user.email}
-                    />
-                  </List.Item>
-                )}
+    <BasePageContainer breadcrumb={breadcrumb} transparent={true}>
+      <Row gutter={24}>
+        <Col
+          className="users-stat-card"
+          xl={6}
+          lg={6}
+          md={12}
+          sm={24}
+          xs={24}
+          style={{ marginBottom: 24 }}
+        >
+          <StatCard
+            loading={loading}
+            icon={<Icon component={AiOutlineTeam} />}
+            title="Users"
+            number={12}
+          />
+        </Col>
+        <Col
+          className="posts-stat-card"
+          xl={6}
+          lg={6}
+          md={12}
+          sm={24}
+          xs={24}
+          style={{ marginBottom: 24 }}
+        >
+          <StatCard
+            loading={loading}
+            icon={<Icon component={MdOutlineArticle} />}
+            title="Posts"
+            number={100}
+          />
+        </Col>
+        <Col xl={6} lg={6} md={12} sm={24} xs={24} style={{ marginBottom: 24 }}>
+          <StatCard
+            loading={loading}
+            icon={<Icon component={BiPhotoAlbum} />}
+            title="Albums"
+            number={100}
+          />
+        </Col>
+        <Col xl={6} lg={6} md={12} sm={24} xs={24} style={{ marginBottom: 24 }}>
+          <StatCard
+            loading={loading}
+            icon={<Icon component={MdOutlinePhoto} />}
+            title="Photos"
+            number={500}
+          />
+        </Col>
+        <Col xl={6} lg={6} md={12} sm={24} xs={24} style={{ marginBottom: 24 }}>
+          <StatCard
+            loading={loading}
+            icon={<Icon component={BiCommentDetail} />}
+            title="Comments"
+            number={500}
+          />
+        </Col>
+        <Col xl={6} lg={6} md={12} sm={24} xs={24} style={{ marginBottom: 24 }}>
+          <StatCard
+            loading={loading}
+            icon={<Icon component={AiOutlineStar} />}
+            title="Reviews"
+            number={100}
+          />
+        </Col>
+        <Col
+          xl={12}
+          lg={12}
+          md={24}
+          sm={24}
+          xs={24}
+          style={{ marginBottom: 24 }}
+        >
+          <Card bordered={false} className="w-full h-full cursor-default">
+            <StatisticCard.Group direction="row">
+              <StatisticCard
+                statistic={{
+                  title: 'XYZ',
+                  value: loading ? 0 : 123,
+                }}
               />
-            </Card>
-          </Col>
-          <Col
-            xl={12}
-            lg={12}
-            md={12}
-            sm={24}
-            xs={24}
-            style={{ marginBottom: 24 }}
-          >
-            <Card bordered={false} className="w-full h-full cursor-default">
-              <Table
-                loading={loading}
-                pagination={false}
-                showHeader={false}
-                dataSource={reviews}
-                columns={[
-                  {
-                    title: 'Title',
-                    dataIndex: 'title',
-                    key: 'title',
-                    align: 'left',
-                  },
-                  {
-                    title: 'Year',
-                    dataIndex: 'year',
-                    key: 'year',
-                    align: 'center',
-                    render: (_, row: Review) => (
-                      <Tag color={row.color}>{row.year}</Tag>
-                    ),
-                  },
-                  {
-                    title: 'Star',
-                    dataIndex: 'star',
-                    key: 'star',
-                    align: 'center',
-                    render: (_, row: Review) => (
-                      <Rate disabled defaultValue={row.star} />
-                    ),
-                  },
-                ]}
+              <StatisticCard
+                statistic={{
+                  title: 'Progress',
+                  value: 'ABC',
+                }}
+                chart={
+                  <Progress
+                    className="text-primary"
+                    percent={loading ? 0 : 75}
+                    type="circle"
+                    size={'small'}
+                    strokeColor={CONFIG.theme.accentColor}
+                  />
+                }
+                chartPlacement="left"
               />
-            </Card>
-          </Col>
-        </Row>
-      </BasePageContainer>
-    </>
+            </StatisticCard.Group>
+          </Card>
+        </Col>
+        <Col
+          xl={12}
+          lg={12}
+          md={12}
+          sm={24}
+          xs={24}
+          style={{ marginBottom: 24 }}
+        >
+          <Card
+            bordered={false}
+            className="users-list w-full h-full cursor-default"
+          >
+            <List
+              loading={loading}
+              itemLayout="horizontal"
+              dataSource={users}
+              renderItem={(user) => (
+                <List.Item>
+                  <List.Item.Meta
+                    avatar={
+                      <Avatar
+                        shape="circle"
+                        size="small"
+                        src={
+                          <LazyImage
+                            src={user.avatar}
+                            placeholder={
+                              <div className="bg-gray-100 h-full w-full" />
+                            }
+                          />
+                        }
+                      />
+                    }
+                    title={`${user.first_name} ${user.last_name}`}
+                    description={user.email}
+                  />
+                </List.Item>
+              )}
+            />
+          </Card>
+        </Col>
+        <Col
+          xl={12}
+          lg={12}
+          md={12}
+          sm={24}
+          xs={24}
+          style={{ marginBottom: 24 }}
+        >
+          <Card
+            className="reviews-table w-full h-full cursor-default"
+            bordered={false}
+          >
+            <Table
+              loading={loading}
+              pagination={false}
+              showHeader={false}
+              dataSource={reviews}
+              columns={[
+                {
+                  title: 'Title',
+                  dataIndex: 'title',
+                  key: 'title',
+                  align: 'left',
+                },
+                {
+                  title: 'Year',
+                  dataIndex: 'year',
+                  key: 'year',
+                  align: 'center',
+                  render: (_, row: Review) => (
+                    <Tag color={row.color}>{row.year}</Tag>
+                  ),
+                },
+                {
+                  title: 'Star',
+                  dataIndex: 'star',
+                  key: 'star',
+                  align: 'center',
+                  render: (_, row: Review) => (
+                    <Rate disabled defaultValue={row.star} />
+                  ),
+                },
+              ]}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </BasePageContainer>
   );
 };
 
