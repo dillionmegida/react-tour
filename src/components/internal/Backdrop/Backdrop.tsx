9661daa04301b27e749f36e4f0b656cabf7cfab9
Dillion Megida <dillionmegida@gmail.com>
2025-04-14 00:22:52 +0200

    fix backdrop zindex not reflecting constant

--- src/components/internal/Backdrop/Backdrop.tsx
+++ src/components/internal/Backdrop/Backdrop.tsx
@@ -1,6 +1,7 @@
 import { createPortal } from 'react-dom';
 import './Backdrop.scss';
+import { BACKDROP_Z_INDEX } from '@/lib/constants';
 
 export function Backdrop() {
-  return createPortal(<div className="tour-backdrop" />, document.body);
+  return createPortal(<div className="tour-backdrop" style={{ zIndex: BACKDROP_Z_INDEX }} />, document.body);
 }
